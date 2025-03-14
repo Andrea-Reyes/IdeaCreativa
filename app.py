dc5ded12c1161bc2ea64f669569783ecec703e7d
Raquel-Reyes <evelinreyeslem@gmail.com>
2025-03-13 21:45:07 -0600

    Creación del index

---
+++ app.py
@@ -0,0 +1,15 @@
+##from flask import Flask, render_template
+
+
+##app = Flask(_name_)
+
+##@app.route("/")
+##def home():
+##    return render_template("index.html")
+
+##@app.route("/contact")
+##def contact():
+##    return render_template("contact.html")
+
+##if _name_ == "_main_":
+##    app.run(debug=True)
